6daea22f219885512e0da76cd35f5d47b08b12f1
the-forest-tree <65894619+the-forest-tree@users.noreply.github.com>
2024-07-29 15:02:33 +0100

    patch object normalization to keep null or undefined attributes

--- src/schemas/Object.js
+++ src/schemas/Object.js
@@ -6,9 +6,7 @@ export const normalize = (schema, input, parent, key, visit, addEntity, visitedE
     const localSchema = schema[key];
     const resolvedLocalSchema = typeof localSchema === 'function' ? localSchema(input) : localSchema;
     const value = visit(input[key], input, key, resolvedLocalSchema, addEntity, visitedEntities);
-    if (value === undefined || value === null) {
-      delete object[key];
-    } else {
+    if (!(value === undefined || value === null)) {
       object[key] = value;
     }
   });
